3b7f96947ee320f81f63aff543fe78a3b1c3d518
ali abbani <aliox@ahlam.com>
2021-11-05 12:08:45 +0200

    Create a list class named MyList that has an instance variable @list, and a method #each that yields successive members of @list and uses the MyEnumerable module.

---
+++ list.rb
@@ -0,0 +1,12 @@
+class MyList
+    include Enumerable
+
+    def initialize(*list)
+        @list = list
+    end
+
+    def each(&block)
+        @list.each do |li|
+            block.call(li)
+    end
+end
